6f7ad39ffc587b02b0f74e97c0c0498f4d2ce4c7
Harsh Gupta <harshgupta88911@gmail.com>
2024-03-09 18:13:49 +0530

    naive approach to find the majority elements

---
+++ Array/MajorityElement/naiveApproach.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int majorityElement(int arr[1000], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        int count = 1;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[i] == arr[j])
+                count++;
+        }
+        if (count > (n / 2))
+            return i;
+    }
+    return -1;
+}
+
+int main()
+{
+    int arr[1000], n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    cout << "Enter the elemnets: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << "Majority elements at index: ";
+    cout << majorityElement(arr, n);
+
+    return 0;
+}
