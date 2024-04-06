6bfec00aef2028c2e6b829711c4f8963079f2a27
Harsh Gupta <harshgupta88911@gmail.com>
2024-04-06 20:26:57 +0530

    add naive approach to solve the sliding wondiw technique

---
+++ Array/SlidingWindowTechnique/naiveApproach.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int maxSum(int arr[], int n, int k)
+{
+    int res = INT_MIN;
+    for (int i = 0; i + k - 1 < n; i++)
+    {
+        int curr = 0;
+        for (int j = 0; j < k; j++)
+        {
+            curr += arr[i + j];
+        }
+        res = max(res, curr);
+    }
+    return res;
+}
+
+int main()
+{
+    int arr[1000], n, res, k;
+    cout << "Enter the number of elemenst: ";
+    cin >> n;
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << "Enter the number of digit you have to add: ";
+    cin >> k;
+    res = maxSum(arr, n, k);
+
+    return 0;
+}
