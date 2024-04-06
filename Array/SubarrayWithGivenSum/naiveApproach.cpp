cb282cf147dd304623a08c4bee403d11c71added
Harsh Gupta <harshgupta88911@gmail.com>
2024-04-06 20:32:12 +0530

    add file of sliding window technique and subarray with given sum

---
+++ Array/SubarrayWithGivenSum/naiveApproach.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int maxSum(int arr[], int n, int k)
+{
+}
+
+int main()
+{
+    int arr[1000], n, res, sum;
+    cout << "Enter the number of elemenst: ";
+    cin >> n;
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << "Enter the number of sum: ";
+    cin >> sum;
+
+    return 0;
+}
