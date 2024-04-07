27426f165633bbca89324777a64305d9e7a8d65b
Harsh Gupta <harshgupta88911@gmail.com>
2024-04-07 19:04:00 +0530

    solve subarray with given sum using naive approach

--- Array/SubarrayWithGivenSum/naiveApproach.js
+++ Array/SubarrayWithGivenSum/naiveApproach.js
@@ -1,14 +1,14 @@
-function isSubSum(arr, sum) {
-  for (let i = 0; i < arr.length; i++) {
-    let curr = 0;
-    for (let j = i; j < arr.length; j++) {
-      curr += arr[j];
-      if (curr === sum) {
-        return true;
-      }
-    }
-  }
-  return false;
-}
+// function isSubSum(arr, sum) {
+//   for (let i = 0; i < arr.length; i++) {
+//     let curr = 0;
+//     for (let j = i; j < arr.length; j++) {
+//       curr += arr[j];
+//       if (curr === sum) {
+//         return true;
+//       }
+//     }
+//   }
+//   return false;
+// }
 
-console.log(isSubSum([1, 4, 20, 3, 10, 5], 33));
+// console.log(isSubSum([1, 4, 20, 3, 10, 5], 33));
