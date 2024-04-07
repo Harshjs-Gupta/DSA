ddcd0d4e57b0666d9e358ccf84e534858394401e
Harsh Gupta <harshgupta88911@gmail.com>
2024-04-07 19:01:50 +0530

    solve subarray with given sum question

---
+++ Array/SubarrayWithGivenSum/efficientApproach.js
@@ -0,0 +1,14 @@
+function isSubSum(arr, sum) {
+  let s = 0,
+    curr = 0;
+  for (let e = 0; e < arr.length; e++) {
+    curr += arr[e];
+    while (sum < curr) {
+      curr -= arr[s];
+      s++;
+    }
+    if (curr === sum) return true;
+  }
+  return false;
+}
+console.log(isSubSum([1, 4, 20, 3, 10, 5], 33));
