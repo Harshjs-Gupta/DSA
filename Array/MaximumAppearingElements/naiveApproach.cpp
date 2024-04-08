ed5604805109f778ad1e472e1cf7d767e67cafad
Harsh Gupta <harshgupta88911@gmail.com>
2024-04-08 18:56:58 +0530

    write a code to find the maximum appearing elements in an array range

--- Array/MaximumAppearingElements/naiveApproach.cpp
+++ Array/MaximumAppearingElements/naiveApproach.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int maxAppear(int left[1000], int right[1000], int n)
+int maxAppear(int left[], int right[], int n)
 {
     int freq[100] = {0};
     for (int i = 0; i < n; i++)
+    {
         for (int j = left[i]; j <= right[i]; i++)
+        {
             freq[j] += 1;
+        }
+    }
     int res = 0;
     for (int i = 1; i < 100; i++)
     {
@@ -21,7 +25,21 @@ int maxAppear(int left[1000], int right[1000], int n)
 int main()
 {
     int left[1000], right[1000], n, res;
-    res = maxAppear([ 1, 2, 4 ], [ 4, 5, 7 ], 3);
+    cout << "Enter the number of elements for left array: ";
+    cin >> n;
+    cout << "Enter the left array elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> left[i];
+    }
+    cout << "Enter the number of elements for right array: ";
+    cin >> n;
+    cout << "Enter the right array elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> right[i];
+    }
+    res = maxAppear(left, right, n);
     cout << "The element that appears maximum times is : " << res;
 
     return 0;
