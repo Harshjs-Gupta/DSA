9ee27d2f12e8a8e0e9941999d992b454a2749f8c
Harsh Gupta <harshgupta88911@gmail.com>
2024-05-06 19:12:45 +0530

    here we find max appearing elements using naive approach

--- Array/MaximumAppearingElements/naiveApproach.cpp
+++ Array/MaximumAppearingElements/naiveApproach.cpp
@@ -5,12 +5,8 @@ int maxAppear(int left[], int right[], int n)
 {
     int freq[100] = {0};
     for (int i = 0; i < n; i++)
-    {
         for (int j = left[i]; j <= right[i]; i++)
-        {
             freq[j] += 1;
-        }
-    }
     int res = 0;
     for (int i = 1; i < 100; i++)
     {
@@ -24,22 +20,9 @@ int maxAppear(int left[], int right[], int n)
 
 int main()
 {
-    int left[1000], right[1000], n, res;
-    cout << "Enter the number of elements for left array: ";
-    cin >> n;
-    cout << "Enter the left array elements: ";
-    for (int i = 0; i < n; i++)
-    {
-        cin >> left[i];
-    }
-    cout << "Enter the number of elements for right array: ";
-    cin >> n;
-    cout << "Enter the right array elements: ";
-    for (int i = 0; i < n; i++)
-    {
-        cin >> right[i];
-    }
-    res = maxAppear(left, right, n);
+    int left[1000], right[1000], res;
+    int n = sizeof(left) / sizeof(left[0]);
+    res = maxAppear({1, 2, 4}, {4, 5, 7}, n);
     cout << "The element that appears maximum times is : " << res;
 
     return 0;
